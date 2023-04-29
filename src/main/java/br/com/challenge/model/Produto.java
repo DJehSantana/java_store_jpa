bd0dcde4b253adad99f990602fdc10c7c1e0e616
Jéssica <jessicah.dias23@hotmail.com>
2023-04-29 12:15:01 -0300

    Criando entidade produto

--- src/main/java/br/com/challenge/model/Produto.java
+++ src/main/java/br/com/challenge/model/Produto.java
@@ -1,10 +1,43 @@
 package br.com.challenge.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
 @Table(name = "produtos")
 public class Produto {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private BigDecimal value;
+
+    public Long getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getValue() {
+        return value;
+    }
+
+    public void setValue(BigDecimal value) {
+        this.value = value;
+    }
 }
